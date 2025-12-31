4589d9312e922d684a42e221ab7e3882a0003715
Youseftemu1 <yousef.s.kutkut@gmail.com>
2025-12-31 17:42:46 +0300

    Finalized production schedules: Hala FM 12:57PM/6PM, JRTV 7PM

--- src/data/systemSchedules.js
+++ src/data/systemSchedules.js
@@ -1,12 +1,32 @@
 export const systemSchedules = [
     {
-        id: 'sys-hala-test-525pm',
-        name: 'Hala FM 5:25PM Test',
+        id: 'sys-hala-short',
+        name: 'Hala FM Daily Short',
         stationId: '2',
         url: 'https://hala-alrayamedia.radioca.st/;',
-        time: '17:25',
+        time: '12:57',
         days: [0, 1, 2, 3, 4, 5, 6],
-        duration: 300,
+        duration: 600,
+        isLocked: true
+    },
+    {
+        id: 'sys-hala-6pm',
+        name: 'Hala FM 6:00 PM',
+        stationId: '2',
+        url: 'https://hala-alrayamedia.radioca.st/;',
+        time: '18:00',
+        days: [0, 1, 2, 3, 4, 5, 6],
+        duration: 600,
+        isLocked: true
+    },
+    {
+        id: 'sys-jrtv-7pm',
+        name: 'JRTV 7:00 PM',
+        stationId: '8',
+        url: 'https://jrtv-live.ercdn.net/jrradio/jordanradiovideo.m3u8',
+        time: '19:00',
+        days: [0, 1, 2, 3, 4, 5, 6],
+        duration: 600,
         isLocked: true
     }
 ];
